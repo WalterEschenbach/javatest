0efbd0b394eaf37c8caa27f59d3521800888d99d
Walter <eschenbach.walter@gmail.com>
2021-10-23 15:01:31 -0500

    initial commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Animal a = new Animal();
+        String d = a.iAmDog();
+        System.out.println(d);
+    }
+
+}
